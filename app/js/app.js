e663b07b9cf537f2b7af76b5abcada6a09aaf830
Jake Schmitz <jake.d.schmitz@gmail.com>
2014-09-14 15:27:57 -0700

    just needs styling now

--- app/js/app.js
+++ app/js/app.js
@@ -1,11 +1,20 @@
-var hashtag = angular.module('hashtag');
+var hashtag = angular.module('hashtag', []);
 
-hashtag.factory('instagram', function($scope, $http) {
+hashtag.factory('Instagram', ['$http', function($http) {
   return {
-    getPics: 1
-  };
-});
+    getHashtag: function(callback) {
+      var endPoint = 'https://api.instagram.com/v1/tags/csulbsigweb/media/recent?client_id=36c08dbf4dc549d3b2f6ea5551dba2e1&callback=JSON_CALLBACK';
 
-hashtag.controller('instaControl', function($scope, instagram) {
-  $scope.something = instagram.getPics;
-});
+      $http.jsonp(endPoint).success(function(res) {
+        callback(res.data);
+      });
+    }
+  };
+}]);
+
+hashtag.controller('InstaControl', ['$scope', '$http', 'Instagram', function($scope, $http, Instagram) {
+  $scope.pics = [];
+  Instagram.getHashtag(function(data) {
+    $scope.pics = data;
+  });
+}]);
